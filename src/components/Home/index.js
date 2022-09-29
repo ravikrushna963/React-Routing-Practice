1b2d09bead608958637ae4f01af384884a3add6b
ravikrushna963 <ravikrushna963@gmail.com>
2022-09-29 10:29:52 +0530

    first commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import "./index.css";
+import React from "react";
+
+const Home = () => (
+  <div className="home-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-blog-img.png"
+      alt="home"
+      className="home-img"
+    />
+    <h1 className="home-heading">Home</h1>
+  </div>
+);
+
+export default Home;
